da06453a588ac4cea04c8a9fedd2b39b98b635b7
Timur <timur.rimsky@gmail.com>
2018-10-14 14:33:25 +0300

    added some specs

--- spec/mxmnd_spec.rb
+++ spec/mxmnd_spec.rb
@@ -5,3 +5,49 @@ describe Mxmnd do
     expect { Mxmnd::city(nil) }.to raise_error Mxmnd::BadRequest
   end
 end
+
+describe 'Mxmnd.city response' do
+	before {
+		url = 'https://geoip.maxmind.com/geoip/v2.1/city/45.17.22.11'
+		path = './spec/tmp/mxmnd_res.txt'
+# i've changed "Content-Length" property of recorded response from 1476 to 2515
+# because of by unknown reason stub didn't return the whole response
+		response_file = File.new(path)
+		stub_request(:get, "http://https//geoip.maxmind.com/geoip/v2.1/city/45.17.22.11:80/").
+          with(# got from webmock hint
+            headers: {  
+       	   'Accept'=>'*/*',
+       	   'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	   'User-Agent'=>'Ruby'
+            }).
+          to_return(response_file) 
+
+		@response = JSON.parse(Net::HTTP.get(url, '/'))
+	}
+
+	it "should be a hash" do
+		@response.is_a?(Hash)
+	end
+
+	it "should have distinct keys" do
+		keys_array = ["city","continent","country","location","maxmind","postal","registered_country","subdivisions","traits"]
+		expect(presence_of_keys?( @response ,keys_array)).to be(true)					
+	end
+	context 'hash' do
+		it 'key city should be a hash including 8 locales' do
+			expect(hash_including_locales(@response["city"], 8)).to be(true)
+		end
+		it 'key continent should be a hash including 8 locales' do
+			expect(hash_including_locales(@response["continent"], 8)).to be(true)
+		end
+		it 'key country should be a hash including 8 locales' do
+			expect(hash_including_locales(@response["country"], 8)).to be(true)
+		end
+		it 'key registered_country should be a hash including 8 locales' do
+			expect(hash_including_locales(@response["registered_country"], 8)).to be(true)
+		end
+		it 'key subdivisions should be an array' do
+			@response["subdivisions"].is_a?(Array)
+		end
+	end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mxmnd"
+require 'webmock/rspec'
+
+def presence_of_keys?(_hash, keys_array)
+  keys_array.each {|key| return false if _hash[key].nil?}
+  true 
+end
+
+def hash_including_locales(_hash, num_of_locales)
+  _hash.is_a?(Hash) && _hash["names"].length == num_of_locales 
+end
