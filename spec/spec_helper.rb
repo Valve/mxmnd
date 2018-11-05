3b85405dae2d4733081add6b757641e9fe220150
Timur <timur.rimsky@gmail.com>
2018-11-05 11:13:23 +0300

    after error correction

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "mxmnd"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'mxmnd'
 require 'webmock/rspec'
 
-def presence_of_keys?(_hash, keys_array)
-  keys_array.each {|key| return false if _hash[key].nil?}
-  true 
+def presence_of_keys?(dict, keys_array)
+  keys_array.each { |key| return false unless dict[key] }
+  true
 end
 
-def hash_including_locales(_hash, num_of_locales)
-  _hash.is_a?(Hash) && _hash["names"].length == num_of_locales 
+def hash_including_locales(dict, num_of_locales)
+  dict.is_a?(Hash) && dict['names'].length == num_of_locales
 end
